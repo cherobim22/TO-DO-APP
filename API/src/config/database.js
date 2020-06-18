17b79468b5b07f4a4249e6d510c67d7e2731fa5d
Lucas Cherobim <lucascherobim@hotmail.com>
2020-06-18 10:54:11 -0300

    update validations

--- API/src/config/database.js
+++ API/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
 
-const url = 'mongodb+srv://cherobim:cherobim@cluster0-xvfly.mongodb.net/test?retryWrites=true&w=majority';
+const url = 'mongodb+srv://yourUser:********@cluster0-xvfly.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoose.connect(url, {useNewUrlParser: true,
     useUnifiedTopology: true});
